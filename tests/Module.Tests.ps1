a96b6b74bb8e6e3d6a33ac1a8ad4662772ae1a10
Marius Storhaug <marstor@hotmail.com>
2024-11-01 23:16:47 +0100

    Remove redundant parameter from tests

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'Module' {
+﻿Describe 'Module' {
     Context "Function: 'Test-PSModuleTemplate'" {
         It 'Should be able to call the function' {
             Test-PSModuleTemplate -Name 'World' | Should -Be 'Hello, World!'
